582af544b4a2266d0aa30c6a6ee6c92d13130714
dusanerdeljan <dusanerdeljan99@gmail.com>
2019-11-28 15:15:32 +0100

    Optimized iteration over board.

--- ConwaysGameOfLife/Main.cpp
+++ ConwaysGameOfLife/Main.cpp
@@ -9,30 +9,23 @@
 #define SCALE 4
 #define UPDATE_RANDOM 0
 #define ALLOW_DRAW 1
+#define SLOW_ANIMATION 1
 
 class Game : public olc::PixelGameEngine
 {
 private:
 	int m_Scale = 4;
-	int** m_Board;
-	int** m_AliveBoard;
+	int* m_Board;
+	int* m_AliveBoard;
 	bool m_SimulationStarted = false;
 public:
-	Game() : m_Board(new int*[SIZE]), m_AliveBoard(new int*[SIZE])
+	Game() : m_Board(new int[SIZE*SIZE]), m_AliveBoard(new int[SIZE*SIZE])
 	{
-		for (int i = 0; i < SIZE; ++i)
-			m_Board[i] = new int[SIZE];
-		for (int i = 0; i < SIZE; ++i)
-			m_AliveBoard[i] = new int[SIZE];
 		sAppName = "Conway's Game of Life";
 	}
 	~Game()
 	{
-		for (int i = 0; i < SIZE; ++i)
-			delete[] m_Board[i];
 		delete[] m_Board;
-		for (int i = 0; i < SIZE; ++i)
-			delete[] m_AliveBoard[i];
 		delete[] m_AliveBoard;
 	}
 private:
@@ -41,36 +34,36 @@ private:
 		for (int i = 0; i < SIZE; i += 1)
 			for (int j = 0; j < SIZE; j += 1)
 			{
-				m_Board[i][j] = 0;
-				m_AliveBoard[i][j] = 0;
+				m_Board[i*SIZE + j] = 0;
+				m_AliveBoard[i*SIZE + j] = 0;
 			}
 	}
 	int CountNeighbours(int x, int y)
 	{
 		int count = 0;
 		if (x > 0 && y > 0)
-			count += m_Board[x - 1][y - 1];
+			count += m_Board[y-1 + (x-1)*SIZE];
 		if (x > 0)
-			count += m_Board[x - 1][y];
+			count += m_Board[y+(x-1)*SIZE];
 		if (x > 0 && y < SIZE-1)
-			count += m_Board[x - 1][y + 1];
+			count += m_Board[y + 1 + (x - 1)*SIZE];
 		if (y > 0)
-			count += m_Board[x][y - 1];
+			count += m_Board[y-1+x*SIZE];
 		if (y < SIZE-1)
-			count += m_Board[x][y + 1];
+			count += m_Board[y + 1 + x*SIZE];
 		if (x < SIZE-1 && y > 0)
-			count += m_Board[x + 1][y - 1];
+			count += m_Board[y-1 + (x+1)*SIZE];
 		if (x < SIZE-1)
-			count += m_Board[x + 1][y];
+			count += m_Board[y+(x+1)*SIZE];
 		if (x < SIZE-1 && y < SIZE - 1)
-			count += m_Board[x + 1][y + 1];
+			count += m_Board[y+1+(x+1)*SIZE];
 		return count;
 	}
 	void ClearAliveBoard()
 	{
 		for (int i = 0; i < SIZE; ++i)
 			for (int j = 0; j < SIZE; ++j)
-				m_AliveBoard[i][j] = 0;
+				m_AliveBoard[i*SIZE + j] = 0;
 	}
 	void RandomPopulation()
 	{
@@ -78,37 +71,33 @@ private:
 		for (int i = 0; i < SIZE; i++)
 			for (int j = 0; j < SIZE; j++)
 			{
-				m_Board[i][j] = rand() % 10 >= 7 ? 1 : 0;
-				if (m_Board[i][j])
-					m_AliveBoard[i][j] = 1;
+				m_Board[i*SIZE + j] = rand() % 10 >= 7 ? 1 : 0;
+				if (m_Board[i*SIZE + j])
+					m_AliveBoard[i*SIZE + j] = 1;
 			}
 	}
 	void UpdatePopulation()
 	{
-		int** newPopulation = new int*[SIZE];
-		for (int i = 0; i < SIZE; ++i)
-			newPopulation[i] = new int[SIZE];
+		int* newPopulation = new int[SIZE*SIZE];
 		for (int x = 0; x < SIZE; x++)
 			for (int y = 0; y < SIZE; y++)
 			{
 				int neighbourCount = CountNeighbours(x, y);
-				if (m_Board[x][y] == 1 && neighbourCount >= 2 && neighbourCount <= 3)
+				if (m_Board[y+x*SIZE] == 1 && neighbourCount >= 2 && neighbourCount <= 3)
 				{
-					newPopulation[x][y] = 1;
-					m_AliveBoard[x][y] = 1;
+					newPopulation[y + x*SIZE] = 1;
+					m_AliveBoard[y + x*SIZE] = 1;
 				}
-				else if (m_Board[x][y] == 1 && neighbourCount > 3)
-					newPopulation[x][y] = 0;
-				else if (m_Board[x][y] == 0 && neighbourCount == 3)
+				else if (m_Board[y + x*SIZE] == 1 && neighbourCount > 3)
+					newPopulation[y + x*SIZE] = 0;
+				else if (m_Board[y + x*SIZE] == 0 && neighbourCount == 3)
 				{
-					newPopulation[x][y] = 1;
-					m_AliveBoard[x][y] = 1;
+					newPopulation[y + x*SIZE] = 1;
+					m_AliveBoard[y + x*SIZE] = 1;
 				}
 				else
-					newPopulation[x][y] = 0;
+					newPopulation[y + x*SIZE] = 0;
 			}
-		for (int i = 0; i < SIZE; ++i)
-			delete[] m_Board[i];
 		delete[] m_Board;
 		m_Board = newPopulation;
 	}
@@ -122,7 +111,7 @@ private:
 			if (m_SimulationStarted)
 				m_SimulationStarted = false;
 #endif
-			m_Board[x][y] = 1;
+			m_Board[y+x*SIZE] = 1;
 		}
 		else if (GetMouse(1).bHeld)
 		{
@@ -130,7 +119,7 @@ private:
 			if (m_SimulationStarted)
 				m_SimulationStarted = false;
 #endif
-			m_Board[x][y] = 0;
+			m_Board[y + x*SIZE] = 0;
 		}
 	}
 public:
@@ -165,7 +154,7 @@ public:
 		for (int x = 0; x < SIZE; x++)
 			for (int y = 0; y < SIZE; y++)
 			{
-				olc::Pixel color = m_Board[x][y] == 1 ? olc::BLACK : (m_AliveBoard[x][y] ? olc::GREEN : olc::WHITE);
+				olc::Pixel color = m_Board[y + x*SIZE] == 1 ? olc::BLACK : (m_AliveBoard[y + x*SIZE] ? olc::GREEN : olc::WHITE);
 				Draw(x, y, color);
 			}
 
@@ -180,9 +169,11 @@ public:
 		if (m_SimulationStarted)
 			UpdatePopulation();
 #endif
+#if SLOW_ANIMATION
 		using namespace std::chrono_literals;
 		if (m_SimulationStarted)
 			std::this_thread::sleep_for(20ms);
+#endif
 		return true;
 	}
 };
